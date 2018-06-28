59e7c1367164cc51d071d3fb9397bf98a5271c17
NataliaMurauyova <natashamur009@gmail.com>
2018-06-28 12:07:20 +0300

    first commit without size classes

---
+++ Autolayout_IB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayout_IB
+//
+//  Created by Наташа on 28.06.18.
+//  Copyright © 2018 Наташа. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
